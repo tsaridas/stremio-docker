eb9cb27212be1e884fc17ccfe726b67abd81b009
Andreas Tsarida <andreas.tsaridas@gmail.com>
2023-12-28 19:16:16 +0100

    rename

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: 'e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */

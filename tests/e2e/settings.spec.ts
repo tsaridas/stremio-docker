7852659c7a0692f81e1a8aab094d047946bae9d9
Andreas Tsarida <andreas.tsaridas@gmail.com>
2024-01-04 00:13:10 +0100

    add tests for troubleshoot

--- tests/e2e/settings.spec.ts
+++ tests/e2e/settings.spec.ts
@@ -1,9 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  //const browser = await chromium.launch();
-  //const context = await browser.newContext();
-  //const page = await context.newPage();
+test('api', async ({ page }) => {
+  // Create a context that will issue http requests.
+  const context = await request.newContext({
+    baseURL: 'http://127.0.0.1:11470',
+  });
+    // Delete a repository.
+  const settings = await context.get(`/settings`, {});
+  expect(settings.ok()).toBeTruthy();
+  expect(settings.json());
+  const settingsText = await settings.text();
+  console.log(`Settings Request Result: ${settingsText}`);
+
+  const network = await context.get(`/network-info`, {});
+  expect(network.ok()).toBeTruthy();
+  expect(network.json());
+  const networkText = await network.text();
+  console.log(`Network Request Result: ${networkText}`);
+});
+
+test('settings', async ({ page }) => {
   await page.goto('http://127.0.0.1:8080/#/settings');
   await page.getByTitle('Streaming').click();
   await page.getByTitle('Configure server url').getByRole('img').click();

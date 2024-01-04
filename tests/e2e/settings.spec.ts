4fdb253ebc0d369e12b250310468fe476c9316b7
Andreas Tsarida <tsaridas@users.noreply.github.com>
2024-01-04 18:39:51 +0100

    Development (#7)
    * small changes

--- tests/e2e/settings.spec.ts
+++ tests/e2e/settings.spec.ts
@@ -1,15 +1,31 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  //const browser = await chromium.launch();
-  //const context = await browser.newContext();
-  //const page = await context.newPage();
-  await page.goto('http://127.0.0.1:8080/#/settings');
+test('api', async ({ page }) => {
+  // Create a context that will issue http requests.
+  const context = await request.newContext({
+    baseURL: 'http://172.17.0.3:11470',
+  });
+    // Delete a repository.
+  const settings = await context.get(`/settings`, {});
+  expect(settings.ok()).toBeTruthy();
+  expect(settings.json());
+  const settingsText = await settings.text();
+  console.log(`Settings Request Result: ${settingsText}`);
+
+  const network = await context.get(`/network-info`, {});
+  expect(network.ok()).toBeTruthy();
+  expect(network.json());
+  const networkText = await network.text();
+  console.log(`Network Request Result: ${networkText}`);
+});
+
+test('settings', async ({ page }) => {
+  await page.goto('http://172.17.0.3:8080/#/settings');
   await page.getByTitle('Streaming').click();
   await page.getByTitle('Configure server url').getByRole('img').click();
   await page.getByPlaceholder('Enter a streaming server url').click();
   await page.getByPlaceholder('Enter a streaming server url').press('Meta+a');
-  await page.getByPlaceholder('Enter a streaming server url').fill('http://127.0.0.1:11470/');
+  await page.getByPlaceholder('Enter a streaming server url').fill('http://172.17.0.3:11470/');
   await page.getByText('Submit').click();
   await expect(page.getByText('Online')).toHaveText('Online');
 });

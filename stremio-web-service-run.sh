a26c752ebb7b9049164866d7cfbd565b424e8554
m4n0v31 <m4n0v31@users.noreply.github.com>
2024-09-15 22:03:14 +0200

    Add fix for incompatible df ouput and curl retry in stremio-web-service-run.sh

--- stremio-web-service-run.sh
+++ stremio-web-service-run.sh
@@ -9,13 +9,16 @@ fi
 grep -q 'self.proxyStreamsEnabled = false,' server.js || sed -i '/self.allTranscodeProfiles = \[\]/a \ \ \ \ \ \ \ \ self.proxyStreamsEnabled = false,' server.js
 
 # fix for incomptible df
-alias df="df -P"
+grep -q 'df -Pk' server.js || sed -i 's/df -k/df -Pk/g' server.js
 
 node server.js &
 sleep 1
 
 if [ ! -z "$IPADDRESS" ]; then 
-	curl "http://localhost:11470/get-https??authKey=&ipAddress=$IPADDRESS"
+	curl --connect-timeout 5 \
+ 	     --retry-all-errors  \
+       	     --retry 5 \
+	     "http://localhost:11470/get-https??authKey=&ipAddress=$IPADDRESS"
 	CERT=$(node extract_certificate.js "$CONFIG_FOLDER")
 	echo "$IPADDRESS" "$CERT" >> /etc/hosts
 	http-server build/ -p 8080 -d false -S -K "$CONFIG_FOLDER""$CERT".pem -C "$CONFIG_FOLDER""$CERT".pem

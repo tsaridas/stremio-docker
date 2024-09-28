d0749740962d5eb731d9d3c0b2442915ce2746c0
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2024-09-28 11:49:45 +0000

    not parallel with firefox

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -23,6 +23,12 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },

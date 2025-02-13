0868b15c8e0dbf9b751cfb426c54299efbfffd24
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2025-02-13 11:01:55 +0000

    reload

--- load_localStorage.js
+++ load_localStorage.js
@@ -9,13 +9,13 @@ async function loadJsonAndStoreInLocalStorage() {
         const data = await response.json();
 
         function processLocalStorageData(data) {
+            let reload = false;
             Object.entries(data).forEach(([key, value]) => {
                 if (!localStorage.getItem(key)) {
                     localStorage.setItem(key, JSON.stringify(value));
                 } else if (key === 'streaming_server_urls') {
                     const existingData = JSON.parse(localStorage.getItem(key) || '{"uid": null, "items": {}}');
                     const newData = value;
-                    
 
                     const mergedItems = {
                         ...existingData.items,
@@ -28,12 +28,17 @@ async function loadJsonAndStoreInLocalStorage() {
                     };
 
                     localStorage.setItem(key, JSON.stringify(mergedData));
+                    reload = true;
                 } else if (key === 'profile') {
                     const existingProfile = JSON.parse(localStorage.getItem(key) || '{}');
                     existingProfile.settings.streamingServerUrl = value.settings.streamingServerUrl;
                     localStorage.setItem(key, JSON.stringify(existingProfile, null, 2));
+                    reload = true;
                 }
             });
+            if (reload) {
+                location.reload();
+            }
         }
 
         processLocalStorageData(data); // Process the data immediately

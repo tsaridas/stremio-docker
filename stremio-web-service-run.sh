f38a6c29e6b16188b6295478b3220048ef4ce0c3
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2025-02-16 18:14:15 +0000

    fix

--- stremio-web-service-run.sh
+++ stremio-web-service-run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
 
-# set the configuration folder path.
 CONFIG_FOLDER="${APP_PATH:-${HOME}/.stremio-server/}"
 
 # check if proxyStreamsEnabled is set to false in server.js and add it if not.
@@ -22,22 +21,19 @@ start_http_server() {
 }
 
 if [ -n "${IPADDRESS}" ]; then 
-    IMPORTED_DOMAIN="$(node certificate.js)"
+    node certificate.js
     EXTRACT_STATUS="$?"
-    IMPORTED_CERT_FILE="${CONFIG_FOLDER}${IMPORTED_DOMAIN}.pem"
-    echo "Extracted domain ${IMPORTED_DOMAIN} with status ${EXTRACT_STATUS} and cert file ${IMPORTED_CERT_FILE}"
 
-    if [ "${EXTRACT_STATUS}" -eq 0 ] && [ -n "${IMPORTED_DOMAIN}" ] && [ -f "${IMPORTED_CERT_FILE}" ]; then
+    if [ "${EXTRACT_STATUS}" -eq 0 ] && [ -f "/srv/stremio-server/certificates.pem" ]; then
         IP_DOMAIN=$(echo $IPADDRESS | sed 's/./-/g')
         echo "${IPADDRESS} ${IP_DOMAIN}.519b6502d940.stremio.rocks" >> /etc/hosts
         cp /etc/nginx/https.conf /etc/nginx/http.d/
-        start_http_server
     else
         echo "Failed to setup HTTPS. Falling back to HTTP."
     fi
 elif [ -n "${CERT_FILE}" ]; then
     if [ -f ${CONFIG_FOLDER}${CERT_FILE} ]; then
-        cp ${CONFIG_FOLDER}${CERT_FILE} /srv/stremio-server/
+        cp ${CONFIG_FOLDER}${CERT_FILE} /srv/stremio-server/certificates.pem
         cp /etc/nginx/https.conf /etc/nginx/http.d/
     fi
 fi

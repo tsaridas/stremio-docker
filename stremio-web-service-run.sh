99cd29e02d0356ce4712ed250b4257952cd52950
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2025-06-27 19:46:13 +0000

    fix

--- stremio-web-service-run.sh
+++ stremio-web-service-run.sh
@@ -12,29 +12,28 @@ fi
 sed -i 's/df -k/df -Pk/g' server.js
 
 if [ -n "${SERVER_URL}" ]; then
-    if [[ "${SERVER_URL: -1}" != "/" ]]; then
+    if [ "${SERVER_URL: -1}" != "/" ]; then
         SERVER_URL="$SERVER_URL/"
     fi
     cp localStorage.json build/localStorage.json
     touch build/server_url.env
-    sed -i "s|http://127.0.0.1:11470/|${SERVER_URL}|g" build/localStorage.json
+    sed -i "s|http://127.0.0.1:11470/|"${SERVER_URL}"|g" build/localStorage.json
 elif [ -n "${AUTO_SERVER_URL}" ] && [ "${AUTO_SERVER_URL}" -eq 1 ]; then
     cp localStorage.json build/localStorage.json
 fi
 
-# Setup authentication if environment variables are set
-if [[ -n "${USERNAME-}" && -n "${PASSWORD-}" ]]; then
+if [ -n "${USERNAME}" ] && [ -n "${PASSWORD}" ]; then
     echo "Setting up HTTP basic authentication..."
-    htpasswd -bc "$HTPASSWD_FILE" "$USERNAME" "$PASSWORD"
-    echo 'auth_basic "Restricted Content";' >$AUTH_CONF_FILE
-    echo 'auth_basic_user_file '"$HTPASSWD_FILE"';' >>$AUTH_CONF_FILE
+    htpasswd -bc "${HTPASSWD_FILE}" "${USERNAME}" "${PASSWORD}"
+    echo 'auth_basic "Restricted Content";' >"${AUTH_CONF_FILE}"
+    echo 'auth_basic_user_file '"${HTPASSWD_FILE}"';' >>"${AUTH_CONF_FILE}"
 else
     echo "No HTTP basic authentication will be used."
 fi
 
 start_http_server() {
-    if [ -n "${WEBUI_INTERNAL_PORT-}" ] && [[ "${WEBUI_INTERNAL_PORT}" =~ ^[0-9]+$ ]] && [ "${WEBUI_INTERNAL_PORT}" -ge 1 ] && [ "${WEBUI_INTERNAL_PORT}" -le 65535 ]; then
-        sed -i "s/8080/${WEBUI_INTERNAL_PORT}/g" /etc/nginx/http.d/default.conf
+    if [ -n "${WEBUI_INTERNAL_PORT}" ] && [ "${WEBUI_INTERNAL_PORT}" -ge 1 ] && [ "${WEBUI_INTERNAL_PORT}" -le 65535 ]; then
+        sed -i "s/8080/"${WEBUI_INTERNAL_PORT}"/g" /etc/nginx/http.d/default.conf
     fi
     nginx -g "daemon off;"
 }
@@ -44,17 +43,16 @@ if [ -n "${IPADDRESS}" ]; then
     EXTRACT_STATUS="$?"
 
     if [ "${EXTRACT_STATUS}" -eq 0 ] && [ -f "/srv/stremio-server/certificates.pem" ]; then
-        IP_DOMAIN=$(echo $IPADDRESS | sed 's/\./-/g')
+        IP_DOMAIN=$(echo "${IPADDRESS}" | sed 's/\./-/g')
         echo "${IPADDRESS} ${IP_DOMAIN}.519b6502d940.stremio.rocks" >> /etc/hosts
         cp /etc/nginx/https.conf /etc/nginx/http.d/default.conf
-
         node certificate.js --action load --pem-path "/srv/stremio-server/certificates.pem" --domain "${IP_DOMAIN}.519b6502d940.stremio.rocks" --json-path "${CONFIG_FOLDER}httpsCert.json"
     else
         echo "Failed to setup HTTPS. Falling back to HTTP."
     fi
 elif [ -n "${CERT_FILE}" ]; then
-    if [ -f ${CONFIG_FOLDER}${CERT_FILE} ]; then
-        cp ${CONFIG_FOLDER}${CERT_FILE} /srv/stremio-server/certificates.pem
+    if [ -f "${CONFIG_FOLDER}${CERT_FILE}" ]; then
+        cp "${CONFIG_FOLDER}${CERT_FILE}" /srv/stremio-server/certificates.pem
         cp /etc/nginx/https.conf /etc/nginx/http.d/default.conf
         node certificate.js --action load --pem-path "/srv/stremio-server/certificates.pem" --domain "${DOMAIN}" --json-path "${CONFIG_FOLDER}httpsCert.json"
     fi

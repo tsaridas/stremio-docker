ebc71dd2a1ecb372dce5aaed282dddcf09685190
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2025-06-27 09:52:30 +0000

    add 0-0-0-0

--- certificate.js
+++ certificate.js
@@ -14,25 +14,29 @@ const crypto = require('crypto');
 
 async function getCertificate() {
   try {
-    const ipAddress = process.env.IPADDRESS;
+    let ipAddress = process.env.IPADDRESS;
+    if (ipAddress === "0-0-0-0") {
+        const publicIp = await fetch('https://api.ipify.org?format=json').then(res => res.json()).then(data => data.ip);
+        ipAddress = publicIp;
+    }
     const response = await fetch('http://api.strem.io/api/certificateGet', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        authKey: null,
-        ipAddress: ipAddress,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            authKey: null,
+            ipAddress: ipAddress,
       }),
     });
 
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status} ${response.statusText}`);
+        throw new Error(`HTTP error! Status: ${response.status} ${response.statusText}`);
     }
 
     const data = await response.json();
     const certResp = JSON.parse(data.result.certificate);
 
     if (!certResp) {
-      throw new Error('Missing certificate or privateKey in API response');
+        throw new Error('Missing certificate or privateKey in API response');
     }
 
     const combinedCertificates = Buffer.from(certResp.contents.Certificate, 'base64').toString() + Buffer.from(certResp.contents.PrivateKey, 'base64').toString();
